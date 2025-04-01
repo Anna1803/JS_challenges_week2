00a79ea71582b918f7f0041bc7c5ae584534ead6
Anna1803 <sandu.anamaria56@yahoo.com>
2025-04-01 10:46:15 +0100

    Add js challenges

---
+++ README.md
@@ -0,0 +1,31 @@
+                                            Software Development Lifecycle
+
+-planning : define purpose + scope ; stakeholders(can be the guvermnet) providing solutions; product managers defining the scope
+
+-Requirement analysis : identify and record; what people want Ex. implement the payment feature for an app
+
+-design an arhitecture : technical plan - tangeable real thing; Ex. which language programming, libraries are we using, data base
+
+-implementation/developing product/QA (quality ansurance) work closelly with the developers
+
+-product testing & integration: Ex.whatsup own by meta which owns more companies => integration ; technical write Ex. dashboards 
+
+-maintenance or deployment and maintenance of products : ex. transitory point very important
+
+
+
+High quality code
+easy to maintain
+refactoring - updating code => making it better 
+
+operation team
+developers = key stakeholders
+
+      PseudoCode
+!Very impprtant - give the computer the right instructions 
+Break down the problem + define the structure of the code: 
+1. data
+2. logic
+3. actions
+
+discovery-student-repos/week-2/tester-SE/javascript-challenges.md
---
+++ js_challenges.js
@@ -0,0 +1,54 @@
+//function that tekes an array and returns the largest number
+
+function largestNumber(arr) {
+    let largest = arr[0]; 
+    
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+
+    return largest;
+}
+
+//console.log(largestNumber([1,6,3,4,5]));
+
+//Write a function that takes a string as input and returns the count of vowels (a, e, i, o, u) in the string.
+
+function countVowels(str) {
+    const vowels = 'aeiouAEIOU';
+    let count = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        if (vowels.includes(str[i])) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+//console.log(countVowels("Hello Mahdi")); 
+
+//Js Coding Challenge -> Begginer level
+
+//Write a function that prints numbers from 1 to n. For multiples of 3, print "Fizz" instead of the number; for multiples of 5, print "Buzz"; and for numbers which are multiples of both 3 and 5, print "FizzBuzz".
+
+function fizzBuzz(n) {
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz")
+        } else if (i % 3 === 0) {
+            console.log("Fizz")
+        } else if (i % 5 === 0) {
+            console.log("Buzz")
+        } else {
+            console.log(i)
+        }
+    }
+}
+
+//fizzBuzz(15); 
+
+//write a function that finds the longest word in a sentence.
